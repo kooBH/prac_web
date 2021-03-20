cd6132c4e1ff3f414a9a705404e49dd616b56b7e
kbh <nine4409@sogang.ac.kr>
2021-03-20 17:02:21 +0900

    first step

---
+++ nodejs/import_square.js
@@ -0,0 +1,2 @@
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
---
+++ nodejs/square.js
@@ -0,0 +1,13 @@
+//exports.area = function(width) { return width * width; };
+//exports.perimeter = function(width) { return 4 * width; };
+
+// equals to 
+module.exports = {
+  area: function(width) {
+    return width * width;
+  },
+
+  perimeter: function(width) {
+    return 4 * width;
+  }
+};
